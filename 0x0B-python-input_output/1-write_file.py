0bb274d803866ca8ee7b1a921ec0b67188ae1cf9
Your Nameroot@896cf839cf9a:/alx-pre_course# git add .root@896cf839cf9a:/alx-pre_course# git commit -m 'My first commit'[master (root-commit) 98eef93] My first commit1 file changed, 1 insertion(+)create mode 100644 README.mdroot@896cf839cf9a:/alx-pre_course# git pushEnumerating objects: 3, done.Counting objects: 100% (3/3), done.Writing objects: 100% (3/3), 212 bytes | 212.00 KiB/s, done.Total 3 (delta 0), reused 0 (delta 0)To https://github.com/Saddamzy001/alx-pre_course.git * [new branch]      master - mastegit config --global user.name Your <saddamhusseini1234@gmail.com>
2023-01-11 10:56:20 -0800

    p

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-"""initializate"""
+def write_file(filename="", text=""):
+    """ function that writes a string to a text file (UTF8)  """
 
-
-def number_of_lines(filename=""):
-    """function that returns the number of lines of a text file"""
-    num = 0
-    with open(filename, encoding="utf-8") as f:
-        line = f.readlines()
-        num = len(line)
-    f.close()
-    return num
+    with open(filename, "w", encoding='utf-8') as f:
+        return f.write(text)
